3898d685606b524ae2364a47330a815107d9fd21
Julio Cesar Caetano <juliocc23@hotmail.com>
2017-12-21 17:57:01 -0200

    Refatoração de código conforme aula 21.

--- ControleEstoque/ControleEstoqueWeb/Controllers/CadastroController.cs
+++ ControleEstoque/ControleEstoqueWeb/Controllers/CadastroController.cs
@@ -8,7 +8,6 @@ namespace ControleEstoque.Web.Controllers
 {
     public class CadastroController : Controller
     {
-
         #region Constantes
         private const int _quantMaxLinhasPorPagina = 5;
         private const string _senhaPadrao = "{$127;$188}";
@@ -70,71 +69,71 @@ namespace ControleEstoque.Web.Controllers
         }
 
         #region Usuários
-      
-        [Authorize]
-        public ActionResult Usuario()
-        {
-            ViewBag.SenhaPadrao = _senhaPadrao;
-            return View(UsuarioModel.RecuperarLista());
-        }
 
-        [HttpPost]
-        [Authorize]
-        [ValidateAntiForgeryToken]
-        public ActionResult RecuperarUsuario(int id)
-        {
-            return Json(UsuarioModel.RecuperarPeloId(id));
-        }
+        //[Authorize]
+        //public ActionResult Usuario()
+        //{
+        //    ViewBag.SenhaPadrao = _senhaPadrao;
+        //    return View(UsuarioModel.RecuperarLista());
+        //}
 
-        [HttpPost]
-        [Authorize]
-        [ValidateAntiForgeryToken]
-        public ActionResult ExcluirUsuario(int id)
-        {
-            return Json(UsuarioModel.ExcluirPeloId(id));
-        }
+        //[HttpPost]
+        //[Authorize]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult RecuperarUsuario(int id)
+        //{
+        //    return Json(UsuarioModel.RecuperarPeloId(id));
+        //}
 
-        [HttpPost]
-        [Authorize]
-        [ValidateAntiForgeryToken]
-        public ActionResult SalvarUsuario(UsuarioModel model)
-        {
-            var resultado = "OK";
-            var mensagens = new List<string>();
-            var idSalvo = string.Empty;
+        //[HttpPost]
+        //[Authorize]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult ExcluirUsuario(int id)
+        //{
+        //    return Json(UsuarioModel.ExcluirPeloId(id));
+        //}
 
-            if (!ModelState.IsValid)
-            {
-                resultado = "AVISO";
-                mensagens = ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage).ToList();
-            }
-            else
-            {
-                try
-                {
-                    if (model.Senha == _senhaPadrao)
-                    {
-                        model.Senha = "";
-                    }
+        //[HttpPost]
+        //[Authorize]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult SalvarUsuario(UsuarioModel model)
+        //{
+        //    var resultado = "OK";
+        //    var mensagens = new List<string>();
+        //    var idSalvo = string.Empty;
 
-                    var id = model.Salvar();
-                    if (id > 0)
-                    {
-                        idSalvo = id.ToString();
-                    }
-                    else
-                    {
-                        resultado = "ERRO";
-                    }
-                }
-                catch (Exception ex)
-                {
-                    resultado = "ERRO";
-                }
-            }
+        //    if (!ModelState.IsValid)
+        //    {
+        //        resultado = "AVISO";
+        //        mensagens = ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage).ToList();
+        //    }
+        //    else
+        //    {
+        //        try
+        //        {
+        //            if (model.Senha == _senhaPadrao)
+        //            {
+        //                model.Senha = "";
+        //            }
 
-            return Json(new { Resultado = resultado, Mensagens = mensagens, IdSalvo = idSalvo });
-        }
+        //            var id = model.Salvar();
+        //            if (id > 0)
+        //            {
+        //                idSalvo = id.ToString();
+        //            }
+        //            else
+        //            {
+        //                resultado = "ERRO";
+        //            }
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            resultado = "ERRO";
+        //        }
+        //    }
+
+        //    return Json(new { Resultado = resultado, Mensagens = mensagens, IdSalvo = idSalvo });
+        //}
 
         #endregion
     }
--- ControleEstoque/ControleEstoqueWeb/Models/UsuarioModel.cs
+++ ControleEstoque/ControleEstoqueWeb/Models/UsuarioModel.cs
@@ -56,7 +56,24 @@ namespace ControleEstoqueWeb.Models
             return ret;
         }
 
-        public static List<UsuarioModel> RecuperarLista()
+        public static int RecuperarQuantidade()
+        {
+            var ret = 0;
+            using (var conexao = new SqlConnection())
+            {
+                conexao.ConnectionString = ConfigurationManager.ConnectionStrings["principal"].ConnectionString;
+                conexao.Open();
+                using (var comando = new SqlCommand())
+                {
+                    comando.Connection = conexao;
+                    comando.CommandText = "select count(*) from usuario";
+                    ret = (int)comando.ExecuteScalar();
+                }
+            }
+            return ret;
+        }
+
+        public static List<UsuarioModel> RecuperarLista(int pagina, int tamPagina)
         {
             var ret = new List<UsuarioModel>();
 
@@ -66,8 +83,11 @@ namespace ControleEstoqueWeb.Models
                 conexao.Open();
                 using (var comando = new SqlCommand())
                 {
+                    var posicao = (pagina - 1) * tamPagina;
                     comando.Connection = conexao;
-                    comando.CommandText = "select * from usuario order by nome";
+                    comando.CommandText = string.Format(
+                        "select * from usuario order by nome offset {0} rows fetch next {1} rows only",
+                        posicao > 0 ? posicao - 1 : 0, tamPagina);
                     var reader = comando.ExecuteReader();
                     while (reader.Read())
                     {
